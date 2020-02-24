0e29014eab6a92eb7882fca00770823a794f18d5
tiago vieira mota <tiago.vieira.mota@gmail.com>
2020-02-23 22:58:06 -0300

    Commit inicial

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="bg-green-300 shadow-lg h-100">
+    <ul class="flex flex-col font-sans mt-8">
+      <li class="p-1 pl-2 " v-for="(route, idx) in rotas" :key="idx">
+        <router-link :to="route.path" active-class="underline text-gray-600">
+          {{route.name}}
+        </router-link>
+        <ul v-if="route.children">
+          <li v-for="(subroute, subidx) in route.children" :key="subidx">
+            <router-link :to="`${route.path}${subroute.path}`">{{subroute.name}}</router-link>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'sidebar',
+    data() {
+      return {
+        rotas: [
+          {
+            path: '/app1',
+            name: 'App1',
+          },
+          {
+            path: '/app2',
+            name: 'App2',
+            children: [
+              {
+                path: '/about',
+                name: 'About',
+              },
+            ]
+          },
+          {
+            path: '/app3',
+            name: 'App3',
+          },
+        ]
+      };
+    },
+  };
+</script>
+
+<style scoped>
+  .grid__container__sidebar {
+    grid-area: 2 / 1 / 6 / 2;
+  }
+</style>
